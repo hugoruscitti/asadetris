e3daccb8ec0ea65c5089459f54f0586eb2e9964e
hugoruscitti <hugoruscitti@localhost>
2009-07-09 22:27:03 -0300

    usando utf8

--- lib/main.py
+++ lib/main.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
 import pygame
 
 
 def run():
+    "Función principal del programa."
 
     screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Asadetris")
+    pygame.display.set_caption("Asadetris - 0.1")
     quit = False
 
     while not quit:
@@ -12,7 +14,6 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit = True
-
 
         pygame.time.delay(10)
 
