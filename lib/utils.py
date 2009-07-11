4a2ac20d902cb77db1f154c44e3611ba8db2fa97
hugoruscitti <hugoruscitti@localhost>
2009-07-11 15:47:02 -0300

    modificando el programa principal para mostrar el titulo del juego.

--- lib/utils.py
+++ lib/utils.py
@@ -3,20 +3,23 @@ import pygame
 import os
 # Como no se aún el directorio donde se ubicaran las imágenes, dejo la variable
 # con una ruta simbólica que más adelante sera sustituida por la auténtica.
-DATOS = 'aqui_ruta_de_imagenes'
+DATOS = os.path.dirname(os.path.abspath(__file__))
 
 # Función para cargar imágenes, devuelve la imagén y su tamaño
 def load_images(name, colorkey = False):
-	fullname = os.path.join(DATOS, name)
+	fullname = os.path.join(DATOS, "../images", name)
 	
 	try: image = pygame.image.load(fullname)
 	except pygame.error, message:
 		print 'No se ha podido cargar la imagen', fullname
 		raise SystemExit, message
 	
-	image = image.convert()
+
 	if colorkey:
 		colorkey = image.get_at((0,0))
 		image.set_colorkey(colorkey, RLEACCEL)
+        else:
+	    image = image.convert_alpha()
+
 	return image, image.get_rect()
 		
