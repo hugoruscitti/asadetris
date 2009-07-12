69c59c10b955fad4292b0921eb29a86249b3ac96
hugoruscitti <hugoruscitti@localhost>
2009-07-12 00:44:11 -0300

    modificando estilo de programacion en utils.py y agregando referencias a modulos en la documentacion.

--- lib/utils.py
+++ lib/utils.py
@@ -8,19 +8,20 @@ DATOS = os.path.dirname(os.path.abspath(__file__))
 
 # Función para cargar imágenes, devuelve la imagen y su tamaño
 def load_images(name, colorkey = False):
-	fullname = os.path.join(DATOS, "../images", name)
-	
-	try: image = pygame.image.load(fullname)
-	except pygame.error, message:
-		print 'No se ha podido cargar la imagen', fullname
-		raise SystemExit, message
-	
+    """Carga una imagen retornando una superficie y su rectangulo.  """
+    fullname = os.path.join(DATOS, "../images", name)
+    
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'No se ha podido cargar la imagen', fullname
+        raise SystemExit, message
+    
 
-	if colorkey:
-		colorkey = image.get_at((0,0))
-		image.set_colorkey(colorkey, RLEACCEL)
-        else:
-	    image = image.convert_alpha()
+    if colorkey:
+        colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    else:
+        image = image.convert_alpha()
 
-	return image, image.get_rect()
-		
+    return image, image.get_rect()
