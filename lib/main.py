6464cbd89736edb7cd3451a11ac7e62115e86915
hugoruscitti <hugoruscitti@localhost>
2009-07-13 23:21:17 -0300

    implementando escenas.

--- lib/main.py
+++ lib/main.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
-import pygame
-import utils
+import director
+import presents_scene
+
 
 
 def run():
     "Función principal del programa."
 
-    screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Asadetris - 0.1")
-    quit = False
-
-    title, rect = utils.load_images("mainmenu/title.png")
-
-    while not quit:
-        screen.fill((200, 200, 200))
-        screen.blit(title, (180, 100))
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit = True
-
-        pygame.display.flip()
-        pygame.time.delay(10)
+    dir = director.Director()
+    scene = presents_scene.PresentsScene(dir)
+    dir.change_scene(scene)
+    dir.loop()
 
 
 if __name__ == '__main__':
