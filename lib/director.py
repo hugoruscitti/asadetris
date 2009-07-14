11cd43a1418f0d8cd666a403e77f875bc53584b5
hugoruscitti <hugoruscitti@localhost>
2009-07-13 23:21:17 -0300

    implementando escenas.

---
+++ lib/director.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+import pygame
+import utils
+
+class Director:
+    """Representa el objeto principal del juego.
+
+    El objeto Director mantiene en funcionamiento el juego, se
+    encarga de actualizar, dibuja y propagar eventos.
+
+    Tiene que utilizar este objeto en conjunto con objetos
+    derivados de Scene."""
+
+    def __init__(self):
+        self.screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Asadetris - 0.1")
+        self.scene = None
+
+    def loop(self):
+        "Pone en funcionamiento el juego."
+
+        quit = False
+
+        while not quit:
+
+            # propaga eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit = True
+                else:
+                    self.scene.on_event(event)
+
+            # actualiza la escena
+            self.scene.on_update()
+            pygame.time.delay(10)
+        
+            # dibuja la pantalla
+            self.screen.fill((200, 200, 200))
+            self.scene.on_draw(self.screen)
+            pygame.display.flip()
+
+
+    def change_scene(self, scene):
+        "Altera la escena actual."
+        self.scene = scene
---
+++ lib/presents_scene.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+import scene
+import utils
+
+class PresentsScene(scene.Scene):
+    """Representa la escena de introducción al juego donde se muestra el logo.
+
+    Esta escena es la primera que se ve al iniciar el juego."""
+
+    def __init__(self, director):
+        scene.Scene.__init__(self, director)
+        self.title, self.rect = utils.load_images("mainmenu/title.png")
+
+    def on_update(self):
+        pass
+
+    def on_draw(self, screen):
+        screen.blit(self.title, (180, 100))
+
+    def on_event(self, event):
+        pass
