5ed1f833cef9b916c8918e478938f040eec226f4
joksnet <joksnet@localhost>
2009-07-19 11:59:56 +0200

    Nuevo Menu para PresentsScene

--- lib/presents_scene.py
+++ lib/presents_scene.py
@@ -2,7 +2,53 @@
 import scene
 import utils
 import pygame
-import game_scene
+# import game_scene
+
+class Menu:
+    def __init__(self, opts, font, color, selected_font, selected_color, margin=0):
+        self.opts = opts
+        self.normal_font = font
+        self.normal_color = color
+        self.selected_font = selected_font
+        self.selected_color = selected_color
+        self.margin = margin
+        
+        self.selected = 0
+        self.imgs_normal = []
+        self.imgs_selected = []
+        
+        line_step = 0
+        
+        for text in self.opts:
+            img_normal, img_normal_size = utils.render_text(text, self.normal_font, self.normal_color)
+            img_selected, img_selected_size = utils.render_text(text, self.selected_font, self.selected_color)
+            
+            self.imgs_normal.append(img_normal)
+            self.imgs_selected.append(img_selected)
+            
+            line_step = max(max(img_normal_size[1], img_selected_size[1]) + self.margin, line_step)
+        
+        self.line_step = line_step
+    
+    def on_draw(self, screen, start_y):
+        center_x = screen.get_width() / 2
+        
+        for i in range(len(self.opts)):
+            if i == self.selected:
+                img = self.imgs_selected[i]
+            else:
+                img = self.imgs_normal[i]
+            
+            x = center_x - img.get_width() / 2
+            y = start_y + self.line_step * i - img.get_height() / 2
+            
+            screen.blit( img, (x,y) )
+    
+    def prev(self):
+        self.selected = (self.selected - 1) % len(self.opts)
+    
+    def next(self):
+        self.selected = (self.selected + 1) % len(self.opts)
 
 class PresentsScene(scene.Scene):
     """Representa la escena de introducción al juego donde se muestra el logo.
@@ -12,22 +58,37 @@ class PresentsScene(scene.Scene):
     def __init__(self, director):
         scene.Scene.__init__(self, director)
         self.title, self.rect = utils.load_images("mainmenu/title.png")
-        self.font = utils.load_font("FreeSans.ttf", 30)
-        msg = "Pulse una tecla para salir"
-        self.text, self.text_size = utils.render_text(msg, self.font)
+        
+        self.menu = Menu(
+            ["Jugar!", "Creditos", "Salir"],
+            utils.load_font("FreeSans.ttf", 30), (0, 0, 0),
+            utils.load_font("FreeSans.ttf", 30), (255, 255, 255)
+        )
+        # msg = "Pulse una tecla para salir"
+        # self.text, self.text_size = utils.render_text(msg, self.font)
 
     def on_update(self):
         pass
 
     def on_draw(self, screen):
         screen.blit(self.title, (180, 20))
-        screen.blit(self.text, (180, 190))
+        # screen.blit(self.text, (180, 190))
+        self.menu.on_draw(screen, 20 + self.title.get_height() + 40)
         
     def on_event(self, event):
-
         if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RETURN:
-				scene = game_scene.GameScene(self.director)
-				self.director.change_scene(scene)
-			elif event.key == pygame.K_ESCAPE:
-				self.director.quit()
+            if event.key == pygame.K_RETURN:
+                # scene = game_scene.GameScene(self.director)
+                # self.director.change_scene(scene)
+                if self.menu.selected == 0:
+                    pass # Jugar!
+                elif self.menu.selected == 1:
+                    pass # Creditos
+                elif self.menu.selected == 2:
+                    self.director.quit()
+            elif event.key == pygame.K_UP:
+                self.menu.prev()
+            elif event.key == pygame.K_DOWN:
+                self.menu.next()
+            elif event.key == pygame.K_ESCAPE:
+                self.director.quit()
