436b5a5011802b3b9c3d59390ccd613fc39164dc
hugoruscitti <hugoruscitti@localhost>
2009-08-23 18:34:19 -0300

    arreglando bug de mi commit anterior (actualizancion de la matriz visual).

--- lib/board.py
+++ lib/board.py
@@ -9,12 +9,16 @@ class Board:
 
     def __init__(self, gamescene):
         self.pieces = []
-        self.visual_matrix = pygame.Surface((10 * 20, 18 * 20), pygame.SRCALPHA, 32)
+        self._create_new_visual_matrix()
         self.matrix = self.init_matrix(10, 18)
         self.gamescene = gamescene
 
     def update(self):
         pass
+
+    def _create_new_visual_matrix(self):
+        size = (10 * 20, 18 * 20)
+        self.visual_matrix = pygame.Surface(size, pygame.SRCALPHA, 32)
 
     #def draw_block(self, row, col):
     #    color = (200, 200, 0)
@@ -93,24 +97,29 @@ class Board:
         rect = (0, row * 20, 10 * 20, 20)
         color = (250, 250, 250)
         self.visual_matrix.fill(color, rect)
-        pass
 
     def chop_line(self, row):
         self._chop_line_matrix(row)
         self._chop_line_block(row)
-        pass
 
     def _chop_line_block(self, row):
-        self.visual_matrix = pygame.transform.chop(self.visual_matrix, pygame.Rect(0, row * 20, 20, 20))
+        rect = pygame.Rect(0, row * 20, 0, 20)
+        new_surface = pygame.transform.chop(self.visual_matrix, rect)
+        self._create_new_visual_matrix()
+        self.visual_matrix.blit(new_surface, (0, 20))
 
     def _chop_line_matrix(self, row):
-        rows = range(0, row)
-        rows.reverse()
+        #rows = range(0, row)
+        #rows.reverse()
         
-        for r in rows:
-            self.matrix[r + 1] = self.matrix[r]
+        #for r in rows:
+        #    self.matrix[r + 1] = self.matrix[r]
         
-        self.matrix[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        #self.matrix[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        empty_line = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.matrix = self.matrix[:row] + self.matrix[row + 1:]
+        self.matrix.insert(0, empty_line)
         
         print "Asi queda la matriz luego de remover la linea."
         import pprint
